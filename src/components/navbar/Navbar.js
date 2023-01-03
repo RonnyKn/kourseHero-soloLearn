40dbc698c7948fe3412287eb52d0e3e6e02c3d1d
RonnyKn <ronnykurniawn@gmail.com>
2023-01-03 14:13:55 +0700

    adding navbar & styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,28 @@ import "./Navbar.css"
 import React from "react"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <section>
+      <div className="container navbar-container">
+        <div className="logo">
+          <h3>Kourse</h3>
+        </div>
+        <div className="navigation">
+          <ul>
+            <li>Home</li>
+            <li>Courses</li>
+            <li>Post a Job</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="signup">
+          <button type="button" className="btn">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Navbar
